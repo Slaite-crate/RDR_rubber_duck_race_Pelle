4d77f1382f0c88c182ec92196b7e3c9f5a10baa6
Pelle <pelle.park@gmail.com>
2020-09-21 22:30:53 +0200

    hello

---
+++ Test/dk/kea/MyQueueTest.java
@@ -0,0 +1,18 @@
+package dk.kea;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import java.util.LinkedList;
+import java.util.Queue;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+public class MyQueueTest {
+    Queue<Object> queueLinkedList = new LinkedList<>();
+
+    @Test
+    void addTest(){
+
+    }
+}
